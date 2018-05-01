f38e3b439752ef65ecd9a8547698e76ce76bf88f
SpaceEEC <wermail2002-kap@yahoo.de>
2018-05-01 07:38:43 +0200

    fix(docs): switch to own fork of ex doc for line breaks

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Crux.Gateway.MixProject do
       {:websockex, "~> 0.4.1"},
       {:poison, "~> 3.1.0"},
       {:credo, "~> 0.9.2", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.18.3", only: :dev}
+      {:ex_doc, git: "https://github.com/spaceeec/ex_doc", only: :dev}
     ]
   end
 end
