9acfde1161fec2cc6538f63fd2d3e8b8bf5376e6
SpaceEEC <spaceeec@yahoo.com>
2018-07-16 13:11:39 +0200

    fix(CI): use new yes flag and environment variable to publish

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,5 @@
 mix deps.get &&
 mix local.hex --force &&
-mkdir -p ~/.hex/ &&
-echo "$HEX_ENCRYPTED_TOKEN" >> ~/.hex/hex.config &&
-echo "$HEX_LOCAL_PASSPHRASE" | mix hex.publish  --no-confirm &&
+mix hex.publish --yes &&
 mix clean &&
 mix deps.clean --all
